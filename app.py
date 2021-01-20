3a9dc3c574b1538a927efa097ea8aba9fdb08761
Dongjin Lee <djlee1509@gmail.com>
2021-01-20 16:40:05 +0100

    Added routing for login and policy page.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/policy')
+def policy():
+    return render_template('policy.html')
 
 
 if __name__ == "__main__":
