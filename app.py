1b2c2ee3eab8c1c9d807c609a48616ca64455111
Dongjin Lee <djlee1509@gmail.com>
2021-01-21 16:41:45 +0100

    Implemented login and policy detail routes logic.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, g
+from flask import Flask, render_template, session, g, request, redirect, url_for
 from model import User
 
 
@@ -9,6 +9,7 @@ users.append(User(user_id=2, username='testuser', password='EbpucVzUP5cvsYha0E9i
 app = Flask(__name__)
 app.secret_key = 'minidemo'
 
+
 @app.before_request
 def before_request():
     g.user = None
@@ -18,13 +19,32 @@ def before_request():
         g.user = user
 
 
-@app.route('/login')
+@app.route('/login', methods=['Get', 'POST'])
 def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+
+        current_users = [x for x in users if x.username == username]
+
+        if not current_users:
+            return redirect(url_for('login'))
+
+        current_user = current_users[0]
+        if current_user and current_user.password == password:
+            session['user_id'] = current_user.id
+            return redirect(url_for('policy'))
+
     return render_template('login.html')
 
 
 @app.route('/policy')
 def policy():
+    if not g.user:
+        return redirect(url_for('login'))
+
     return render_template('policy.html')
 
 
