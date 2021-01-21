66ef0dd0f0f837fc9dd59db448a2bfd5ab4c8d53
Dongjin Lee <djlee1509@gmail.com>
2021-01-21 16:10:24 +0100

    Added seed users.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, g
+from model import User
 
+
+users = []
+users.append(User(user_id=1, username='jacob', password='secretword'))
+users.append(User(user_id=2, username='testuser', password='EbpucVzUP5cvsYha0E9i'))
 
 app = Flask(__name__)
+app.secret_key = 'minidemo'
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
 
 
 @app.route('/login')
---
+++ model.py
@@ -0,0 +1,8 @@
+import requests
+
+
+class User:
+    def __init__(self, user_id, username, password):
+        self.id = user_id
+        self.username = username
+        self.password = password
